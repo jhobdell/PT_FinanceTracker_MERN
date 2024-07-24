6c49b7fd3130f252ecd85b3dab9ffb8202c9c001
Jonathan <phyogaiom@gmail.com>
2024-07-24 11:59:34 +0100

    first commit

---
+++ client/src/pages/dashboard/index.tsx
@@ -0,0 +1,16 @@
+import { useUser } from "@clerk/clerk-react";
+import { FinancialRecordForm } from "./FinancialRecordForm";
+import { FinancialRecordList } from "./FinancialRecordList";
+
+export const Dashboard = () => {
+
+    const {user} = useUser();
+
+    return (
+        <div className="dashboard-container">
+            <h1>Welcome, {user?.firstName}!  Here are you finances.</h1>
+            <FinancialRecordForm />
+            <FinancialRecordList />
+        </div>
+    )
+};
---
+++ server/src/index.ts
@@ -0,0 +1,7 @@
+//password:9A3fDT7UdzkFhwHI
+//username: Jonathan
+
+import express, {Express} from 'express';
+import mongoose from 'mongoose';
+
+const app: Express = express();
